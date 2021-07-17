7260c6b31cc5b22c4807b8d5ed008d1cbcaf6529
Arnav Gupta <arnavrgupta002@gmail.com>
2021-07-17 13:34:17 +0530

    theme-toggling enabled

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
+import DarkModeToggle from 'react-dark-mode-toggle';
+import { useState } from 'react';
 import Card from './components/Card.js';
 import data from './assets/data.js';
 
 function App() {
+	const [isDarkMode, setIsDarkMode] = useState(false);
+	const lightTheme = {
+		backgroundColor: '#fff',
+		color: '#000',
+	};
+
+	const darkTheme = {
+		backgroundColor: '#000',
+		color: '#fff',
+	};
+
 	return (
-		<div className="App">
+		<div className="App" style={isDarkMode ? darkTheme : lightTheme}>
 			<div className="header">
 				<h2 id="header">Nav's Socials</h2>
 				<p className="subtitle">By Nav, 2021.</p>
+				<DarkModeToggle
+					className="dark-mode-toggle"
+					onChange={setIsDarkMode}
+					checked={isDarkMode}
+					size={70}
+				/>
 			</div>
 
 			<div className="cards-container">
